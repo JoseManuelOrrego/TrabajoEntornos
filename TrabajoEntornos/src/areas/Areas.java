3c8dbc2537c099e6b0e327d7bd4f56620068f597
JoseManuelOrrego <jmobejarano@gmail.com>
2024-01-11 22:13:36 +0100

    area del triangulo

--- TrabajoEntornos/src/areas/Areas.java
+++ TrabajoEntornos/src/areas/Areas.java
@@ -1,10 +1,24 @@
 package areas;
 
-public class Areas {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Areas 
+{
+	public static void main(String[] args) 
+	{
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Introduce la base del triangulo: ");
+		double base = entrada.nextDouble();
+		System.out.println("Introduce la altura del triangulo: ");
+		double altura = entrada.nextDouble();
+		
+		double resultado = calcularAreaTriangulo(base, altura);
+		System.out.println("El area del triangulo es: " + resultado);
 	}
-
+	
+	public static double calcularAreaTriangulo(double base, double altura) 
+	{
+		return (base*altura)/2;
+	}
 }
